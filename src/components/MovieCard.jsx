ce26dda3e6ec96a7b6124f65d2a332faaf89703a
Aswajit Mohapatra <aswajit.cool123@gmail.com>
2023-03-20 20:15:16 +0530

    first commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MovieCard = ({ myData }) => {
+    console.log(myData)
+  const { title, body, id } = myData;
+  return (
+    <div className="card">
+      <div className="card-info">
+        <p className="card-id">{id}</p>
+        <p>{body.substr(0, 150)}</p>
+        <h2>{title.substr(0, 15)}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
